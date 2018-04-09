a192c529b6f0a6695797947844a5362016e851a8
Remfath Chan <remath@gmail.com>
2018-04-09 22:05:04 +0800

    Show cli options when start app

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"time"
-	"log"
-	"gopkg.in/robfig/cron.v2"
+	"github.com/remfath/procroner/cli"
 )
 
 func main() {
-	Croner := cron.New()
-	Croner.AddFunc("* * * * * *", func() {
-		f, err := os.Create(fmt.Sprintf("%s/%s.txt", os.TempDir()+"/test", time.Now().Format(time.RFC3339)))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-	})
-	Croner.Start()
+	cli.Show()
 }
